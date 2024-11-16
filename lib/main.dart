ad5b7b9d166e24691469c8e83086654e2e484344
TCHANHOUIN <aulerien80@gmail.com>
2024-11-16 14:13:52 +0100

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -10,6 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // whenever your initialization is completed, remove the splash screen:
+    FlutterNativeSplash.remove();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
