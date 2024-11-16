f7408a6785ca0129b8e87ff09b505023a925a2c2
TCHANHOUIN <aulerien80@gmail.com>
2024-11-16 14:32:20 +0100

    icon laucher

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color appColorPink = Color(0xffdc05e4);
+  static const Color appColorGray = Color(0xff444c6c);
+  static const Color appColorPinkLight = Color(0xffb82ce8);
+  static const Color appColorViolet = Color(0xff682cac);
+  static const Color appColorVioletLight = Color(0xff682cac);
+  static const Color appColorChocholate = Color(0xff6c286c);
+}
